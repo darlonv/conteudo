b328e85c2c3dd0e953e361e165fc4602d1743bec
Darlon <darlonv@gmail.com>
2024-03-27 19:18:37 -0300

    update
    set -x incluido

--- Docker/build_manifest.sh
+++ Docker/build_manifest.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
+
+
 ARCHNAME_A="amd64"
-ARCHNAME_b="arm64"
+ARCHNAME_B="arm64"
 
 TAG=latest
 
+set -x
 docker manifest rm darlon/myst:${TAG} #remove possiveis manifests anteriores
 docker manifest create darlon/myst:${TAG} darlon/myst-${ARCHNAME_A}:${TAG} darlon/myst-${ARCHNAME_B}:${TAG}
 docker manifest push darlon/myst:${TAG}
+set +x
 
 TAG=full
 
+set -x
 docker manifest rm darlon/myst:${TAG} #remove possiveis manifests anteriores
 docker manifest create darlon/myst:${TAG} darlon/myst-${ARCHNAME_A}:${TAG} darlon/myst-${ARCHNAME_B}:${TAG}
 docker manifest push darlon/myst:${TAG}
+
+set +x
