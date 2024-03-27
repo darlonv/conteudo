2ef31bea005ce47e4d89b35afda28ef0954145ed
Darlon <darlonv@gmail.com>
2024-03-27 19:14:16 -0300

    correção

--- Docker/build_manifest.sh
+++ Docker/build_manifest.sh
@@ -6,11 +6,11 @@ ARCHNAME_b="arm64"
 TAG=latest
 
 docker manifest rm darlon/myst:${TAG} #remove possiveis manifests anteriores
-docker manifest create darlon/myst:full darlon/myst-${ARCHNAME_A}:${TAG} darlon/myst-${ARCHNAME_B}:${TAG}
+docker manifest create darlon/myst:${TAG} darlon/myst-${ARCHNAME_A}:${TAG} darlon/myst-${ARCHNAME_B}:${TAG}
 docker manifest push darlon/myst:${TAG}
 
 TAG=full
 
 docker manifest rm darlon/myst:${TAG} #remove possiveis manifests anteriores
-docker manifest create darlon/myst:full darlon/myst-${ARCHNAME_A}:${TAG} darlon/myst-${ARCHNAME_B}:${TAG}
+docker manifest create darlon/myst:${TAG} darlon/myst-${ARCHNAME_A}:${TAG} darlon/myst-${ARCHNAME_B}:${TAG}
 docker manifest push darlon/myst:${TAG}
